1f376462042c578e8fbd69fc0410ed4b04f75f00
AdrianSanchez1814 <adrian_sanchez1@epamneoris.com>
2026-07-06 13:18:47 +0200

    Add main App component

---
+++ customer-success-dashboard/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Dashboard from './components/Dashboard/Dashboard';
+
+/**
+ * App Component
+ * Componente principal de la aplicación Customer Success Dashboard
+ */
+function App() {
+  return (
+    <div className="app">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
